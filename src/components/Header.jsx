a3e04df3fc8f960d3010f65809c44c368e844540
Mauricio Sauza <mauriciosauzatorrez666@gmail.com>
2023-10-19 23:38:56 -0400

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+      <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>
+        Patient status &nbsp;
+        <span className='text-indigo-600'>Vet Clinic</span>
+      </h1>
+    </>
+  )
+}
